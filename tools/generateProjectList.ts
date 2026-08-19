98063cd12388a88879b10c3dc8aec0674bdb7409
cdcarr <cdcarr2001@gmail.com>
2026-08-19 15:34:55 -0400

    Generate projects list on run and build

---
+++ tools/generateProjectList.ts
@@ -0,0 +1,22 @@
+import fs from 'fs';
+
+/**
+ * Read directories from public/projects, filter out the example and projects.json, and write to
+ * public json
+ */
+function generateProjectList(): void {
+
+    // Get items in projects directory
+    let items = fs.readdirSync(`./public/projects`);
+
+    // Filter out example project and projects json
+    items = items.filter(item => !['ex_project', 'projects.json'].includes(item));
+
+    // Write to a json
+    fs.writeFileSync(
+        './public/projects/projects.json',
+        JSON.stringify(items)
+    );
+}
+
+generateProjectList();
