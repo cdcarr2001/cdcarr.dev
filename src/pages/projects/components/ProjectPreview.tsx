549754ade88b207f2a354e90dedce11d7d6c5e33
cdcarr <cdcarr2001@gmail.com>
2026-08-23 17:26:07 -0400

    Fix odd issue where live site could not find project json without using ? on the instance before the concat

--- src/pages/projects/components/ProjectPreview.tsx
+++ src/pages/projects/components/ProjectPreview.tsx
@@ -3,13 +3,14 @@ import type { ProjectJson } from "../types/ProjectJson";
 import { NavLink } from "react-router";
 
 // TODO document
+// TODO respect line character limit
 
 export default function ProjectPreview(
     props: ProjectPreviewProps
 ): ReactElement {
 
     /** Languages used in the project */
-    const languages: string[] = props.projectJson.programmingLanguages.concat(props.projectJson.otherLanguages);
+    const languages: string[] = props.projectJson.programmingLanguages?.concat(props.projectJson.otherLanguages);
     /** Libraries used in the project */
     const libraries: string[] = props.projectJson.libraries;
     /** Frameworks used in the project */
