b687f2aecbf3db18c31534ed5bca5d8bbb42c893
cdcarr <cdcarr2001@gmail.com>
2026-08-23 17:11:26 -0400

    Update project preview to attatch languages, libraries, frameworks, and technologies to the dataset of the html element

--- src/pages/projects/components/ProjectPreview.tsx
+++ src/pages/projects/components/ProjectPreview.tsx
@@ -8,18 +8,36 @@ export default function ProjectPreview(
     props: ProjectPreviewProps
 ): ReactElement {
 
+    /** Languages used in the project */
+    const languages: string[] = props.projectJson.programmingLanguages.concat(props.projectJson.otherLanguages);
+    /** Libraries used in the project */
+    const libraries: string[] = props.projectJson.libraries;
+    /** Frameworks used in the project */
+    const frameworks: string[] = props.projectJson.frameworks;
+    /** Technologies used in the project */
+    const technologies: string[] = props.projectJson.otherTechnologies;
+
     return(
-        <NavLink
-            to={`/projects/${props.projectJson.name}`}
-            className='navlink'
+        <div
+            id={`project-${props.projectJson.name}-preview`}
+            key={`project-${props.projectJson.name}-preview`}
+            className='project-preview-container'
+            data-languages={languages}
+            data-libraries={libraries}
+            data-frameworks={frameworks}
+            data-technologies={technologies}
         >
-            <div
-                id={`project-${props.projectJson.name}-preview`}
-                key={`project-${props.projectJson.name}-preview`}
+            <NavLink
+                to={`/projects/${props.projectJson.name}`}
+                className='navlink'
             >
-                {props.projectJson.name}
-            </div>
-        </NavLink>
+                <div
+                    className='project-preview'
+                >
+                    {props.projectJson.name}
+                </div>
+            </NavLink>
+        </div>  
     );
 }
 
