4497e2b946ccb06028166054a9d57e96caee13b6
cdcarr <cdcarr2001@gmail.com>
2026-08-30 14:53:28 -0400

    Update to check if json is obtained

--- src/pages/projects/constants/ProjectMap.ts
+++ src/pages/projects/constants/ProjectMap.ts
@@ -34,7 +34,14 @@ async function fetchProjects(): Promise<Map<string, ProjectJson>> {
                 .then((response) => {
                     if (response.ok) {
 
-                        return(response.json());
+                        if (response.headers.get("content-type") == 'application/json') {
+
+                            return(response.json());
+                        }
+                        else {
+
+                            return(null);
+                        }
                     }
                     else {
 
