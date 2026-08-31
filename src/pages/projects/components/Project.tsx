f284438acbd6baa035cf47406fb78773e77b740d
cdcarr <cdcarr2001@gmail.com>
2026-08-30 20:19:24 -0400

    Begin work on project page

--- src/pages/projects/components/Project.tsx
+++ src/pages/projects/components/Project.tsx
@@ -2,6 +2,7 @@ import type { ReactElement } from "react";
 import { useParams } from "react-router";
 import { PROJECT_MAP } from "../constants/ProjectMap";
 import ProjectNotFound from "./ProjectNotFound";
+import ImageGallery from "../../../components/image_gallery/ImageGallery";
 
 // TODO document
 // TODO fix missing unique key error
@@ -20,11 +21,74 @@ export default function Project(): ReactElement {
         return(<ProjectNotFound/>);
     }
 
+    /** Languages used in the project */
+    const languages: string[] = projectJson.programmingLanguages?.concat(projectJson.otherLanguages);
+    /** Libraries used in the project */
+    const libraries: string[] = projectJson.libraries;
+    /** Frameworks used in the project */
+    const frameworks: string[] = projectJson.frameworks;
+    /** Technologies used in the project */
+    const technologies: string[] = projectJson.otherTechnologies;
+
     return(
-        <div>
-            <h1>
-                {projectJson.name}
-            </h1>
+        <div
+            id='project'
+        >
+            <div
+                className='project-images'
+            >
+                <ImageGallery
+                    imagePaths={projectJson.extraImages}
+                />
+            </div>
+            <div
+                className='header'
+            >
+                <h1>
+                    {projectJson.name}
+                </h1>
+            </div>
+            <div
+                className='project-information'
+            >
+                {
+                    languages.length > 0 &&
+                    <div>
+                        {`Languages: ${languages.join(', ')}`}
+                    </div>
+                }
+                {
+                    libraries.length > 0 &&
+                    <div>
+                        {`Libraries: ${libraries.join(', ')}`}
+                    </div>
+                }
+                {
+                    frameworks.length > 0 &&
+                    <div>
+                        {`Frameworks: ${frameworks.join(', ')}`}
+                    </div>
+                }
+                {
+                    technologies.length > 0 &&
+                    <div>
+                        {`Technologies: ${technologies.join(', ')}`}
+                    </div>
+                }
+            </div>
+            <div
+                className='project-description'
+            >
+                <h4>Description:</h4>
+                {projectJson.desc}
+            </div>
         </div>
     );
 }
+/*
+
+            <img
+                className='project-banner'
+                src={projectJson.bannerImage ? projectJson.bannerImage : undefined}
+            />
+*/
